a38597bef231a72b99e1bec78f03f0f3fdf13d68
Thiago Nestor <thiagonestorafonso@gmail.com>
2026-04-22 16:20:01 -0300

    atts

--- Protótipos/GROWLY DATABASE.sql
+++ Protótipos/GROWLY DATABASE.sql
@@ -3,63 +3,63 @@ create database growly;
 use growly;
 
 CREATE TABLE USUARIO (
-id int unique primary key,
-nome varchar(400),
-email text,
-ursn varchar(20),
-senha int(8) unique
+	id int unique primary key,
+	nome varchar(400),
+	email text,
+	ursn varchar(20),
+	senha int(8) unique
 );
 
 CREATE TABLE PANC(
-id_panc int unique primary key,
-n_PANC varchar(111),
-infos text,
-categ varchar(100)
+	id_panc int unique primary key,
+	n_PANC varchar(111),
+	infos text,
+	categ varchar(100)
 );
 
 CREATE TABLE IDENTIFICACAO(
-id_iden int unique primary key,
+	id_iden int unique primary key,
 
-id int,
-id_panc int,
-foreign key (id) references USUARIO(id),
-foreign key (id_panc) references PANC(id_panc),
+	id int,
+	id_panc int,
+	foreign key (id) references USUARIO(id),
+	foreign key (id_panc) references PANC(id_panc),
 
-dat date,
-hora time,
-padr text,
-p_d_conf int
+	dat date,
+	hora time,
+	padr text,
+	p_d_conf int
 );
 
 CREATE TABLE JARDIM (
-id_jardim int unique primary key,
+	id_jardim int unique primary key,
 
-id int,
-foreign key (id) references USUARIO(id),
+	id int,
+	foreign key (id) references USUARIO(id),
 
-resg text
+	resg text
 );
 
 CREATE TABLE JARDIM_PANC(
-id_jardim int,
-id_panc int,
-foreign key (id_jardim) references JARDIM(id_jardim),
-foreign key (id_panc) references PANC(id_panc),
+	id_jardim int,
+	id_panc int,
+	foreign key (id_jardim) references JARDIM(id_jardim),
+	foreign key (id_panc) references PANC(id_panc),
 
-quantidade text,
-dat_add date
+	quantidade text,
+	dat_add date
 );
 
 CREATE TABLE MENSAGEM(
-id_msg int unique primary key,
+	id_msg int unique primary key,
 
-id int,
-id_iden int,
-foreign key (id) references USUARIO(id),
-foreign key (id_iden) references IDENTIFICACAO(id_iden),
+	id int,
+	id_iden int,
+	foreign key (id) references USUARIO(id),
+	foreign key (id_iden) references IDENTIFICACAO(id_iden),
 
-txt text,
-resp text,
-dat date,
-hora time
+	txt text,
+	resp text,
+	dat date,
+	hora time
 );
